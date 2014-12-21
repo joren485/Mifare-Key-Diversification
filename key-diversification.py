ad36063b29384435d12a7279cc2e7f0d5ff0c31d
Joren Vrancken <jorenvrancken@gmail.com>
2014-12-21 20:52:11 +0100

    Nice-ify

--- key-diversification.py
+++ key-diversification.py
@@ -31,23 +31,23 @@ def xor(data, key):
     return xored.encode("hex")
 
 def cmac_div(key, UID, Sector_number):
-    ##init parameters en variablen
+    """"""
     from Crypto.Cipher import AES
 
-    IV="00000000000000000000000000000000".decode("hex") ##Init vector for AES
-    cipher = AES.new(key.decode("hex"),AES.MODE_CBC,IV) ##AES in Cipher block Chaining mode met Init Vector=00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
-    K1,K2=gen_subkeys(key,cipher)
-    xorkey=K1
+    IV = "00000000000000000000000000000000".decode( "hex" ) ##Init vector for AES
+    cipher = AES.new(key.decode( "hex" ), AES.MODE_CBC, IV) ##AES in Cipher block Chaining mode, Init Vector=00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+    K1, K2 = gen_subkeys( key, cipher )
+    xorkey = K1
 
     M = "01" + UID + Sector_number
     padding = "8000000000000000000000000000000000000000000000000000"
 
-    if len(M) < 64: ## check of padding nodig is
+    if len(M) < 64: ## if padding needed
             M += padding
             xorkey = K2
 
     if len(M) != 64:
-            print "M niet 32 byte!"
+            print "M != 32 byte!"
             exit()
 
     xordata = M[-32:].decode("hex")##last 16 bytes of M
@@ -58,20 +58,21 @@ def cmac_div(key, UID, Sector_number):
     divkey = cipher.encrypt( M.decode( "hex" ) ).encode( "hex" )[-32:-20] ##AES M and slice out the right piece
 
     print "AES version"
-    print "Masterkey:\t "+ key.upper()
-    print "UID:\t\t "+UID.upper()
-    print "Sector:\t\t "+Sector_number.upper()
+    print "Masterkey:\t " + key.upper()
+    print "UID:\t\t " + UID.upper()
+    print "Sector:\t\t " + Sector_number.upper()
     print "Subkey 1:\t " + K1.encode("hex").upper()
     print "Subkey 2:\t " + K2.encode("hex").upper()
-    print "Message:\t "+M.upper()
+    print "Message:\t " + M.upper()
     print "Diversified key: " + divkey.upper()
     print 
 
     return divkey
 
 def des3_div(key, UID, Sector_number, MIFkey):
-    
+    """"""
     from Crypto.Cipher import DES3
+
     trailerblock = 4*int(Sector_number)+3 ##van sector naar trailerblock van sector
     trailerblock = "{:02x}".format(trailerblock)
 
@@ -85,19 +86,19 @@ def des3_div(key, UID, Sector_number, MIFkey):
     divkey=cipher.encrypt( M.decode( "hex" ) ).encode( "hex" )[2:14]
 
     print "3DES version"
-    print "Masterkey:\t "+ key.upper()
-    print "UID:\t\t "+UID.upper()
-    print "Sector:\t\t "+Sector_number.upper()
-    print "Trailer Block:\t "+trailerblock
-    print "Mifare key:\t "+MIFkey.upper()
-    print "Message:\t "+M.upper()
+    print "Masterkey:\t " + key.upper()
+    print "UID:\t\t " + UID.upper()
+    print "Sector:\t\t " + Sector_number.upper()
+    print "Trailer Block:\t " + trailerblock
+    print "Mifare key:\t " + MIFkey.upper()
+    print "Message:\t " + M.upper()
     print "Diversified key: " + divkey.upper()
     print
+
     return divkey
 
 if __name__ == "__main__":
 
-    ### Test data 
     masterkey = "00112233445566778899aabbccddeeff"
     UID="F4EA548E"
     Sector_number="05"
